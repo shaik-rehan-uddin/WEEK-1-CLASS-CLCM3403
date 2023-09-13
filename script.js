4e47e9cabd014ca78f76c7ca18bb612c40574454
Rehanuddin Shaik <144375108+shaik-rehan-uddin@users.noreply.github.com>
2023-09-13 14:43:20 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("runaway-div");
+
+const animateMove = (element, prop, pixels) =>
+  anime({
+    targets: element,
+    [prop]: `${pixels}px`,
+    easing: "easeOutCirc"
+  });
+
+["mouseover", "click"].forEach(function (el) {
+  button.addEventListener(el, function (event) {
+    const top = getRandomNumber(window.innerHeight- 50 - this.offsetHeight);
+    const left = getRandomNumber(window.innerWidth- 50 - this.offsetWidth);
+
+    animateMove(this, "left", left).play();
+    animateMove(this, "top", top).play();
+  });
+});
+
+const getRandomNumber = (num) => {
+  return Math.floor(Math.random() * (num + 1));
+};
